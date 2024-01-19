e4ca5f3b284900395c31de1e14f0cccb49867234
titaotalora <andreaotalora9509@gmail.com>
2024-01-19 18:18:07 -0500

    Se crea el archivo  de practica en Codedéx

--- hello.py
+++ hello.py
@@ -1,1 +1,19 @@
-print('Hello world')
+#Este es un comentario
+# Nuestro Hola mundo en python
+print('Hola Python')
+print("Hola Python")
+
+print(type("Hola Python"))
+
+
+"""
+Este es un comentario
+en varias
+líneas
+"""
+'''
+Este también es un 
+comentario
+en varias
+líneas
+'''
---
+++ pattern.py
@@ -0,0 +1,6 @@
+
+#Ejercicio de Codedéx 03. Pattern
+print('       1')
+print('     2 3 ')
+print('   4 5 6 ')
+print('7 8 9 10 ')
